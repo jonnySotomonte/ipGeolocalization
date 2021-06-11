36014a91033e0eab9b4a1ce888a842acc6f3e4f4
jonnySotomonteSeafair <jonny@seafair.io>
2021-06-11 17:27:54 -0500

    initial commit

---
+++ src/main/java/com/meli/ipgeolocalization/IpGeoLocalizationApplication.java
@@ -0,0 +1,13 @@
+package com.meli.ipgeolocalization;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IpGeoLocalizationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IpGeoLocalizationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/meli/ipgeolocalization/IpGeoLocalizationApplicationTests.java
@@ -0,0 +1,8 @@
+package com.meli.ipgeolocalization;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication(scanBasePackages = {"com.meli.ipgeolocalization"})
+class IpGeoLocalizationApplicationTests {
+
+}
