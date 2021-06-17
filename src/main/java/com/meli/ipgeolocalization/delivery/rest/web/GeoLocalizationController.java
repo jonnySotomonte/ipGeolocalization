7f1c0fcea943a04e39aaedcdd5589f327fa4b820
jonnySotomonteSeafair <jonny@seafair.io>
2021-06-17 12:23:29 -0500

    Running app on local environment

--- src/main/java/com/meli/ipgeolocalization/delivery/rest/web/GeoLocalizationController.java
+++ src/main/java/com/meli/ipgeolocalization/delivery/rest/web/GeoLocalizationController.java
@@ -26,7 +26,7 @@ public class GeoLocalizationController {
   private final QueryHistoryStorage historyStorage;
 
   public GeoLocalizationController(IpTracer ipTracer,
-      @Qualifier(value = "dynamoHistory") QueryHistoryStorage historyStorage) {
+      @Qualifier(value = "localHistory") QueryHistoryStorage historyStorage) {
     this.ipTracer = ipTracer;
     this.historyStorage = historyStorage;
   }
